b6a89448142fc57722c4238721d1988bdac6062f
kostis30fyllou <cs2180018@di.uoa.gr>
2019-01-09 14:59:47 +0200

    Initial commit

---
+++ m149_p2/src/main/java/gr/uoa/di/m149_p2/M149P2Application.java
@@ -0,0 +1,14 @@
+package gr.uoa.di.m149_p2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M149P2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M149P2Application.class, args);
+    }
+
+}
+
